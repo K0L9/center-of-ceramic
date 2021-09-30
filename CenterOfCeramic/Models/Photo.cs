e0724e57225f6899ea06f79ea495748b9912af28
K0L9 <kovalkola2@gmail.com>
2021-09-30 23:00:21 +0300

    Init

---
+++ CenterOfCeramic/Models/Photo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CenterOfCeramic.Models
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+        public string URL { get; set; }
+
+        //Foriegn key
+        public int ProductId { get; set; }
+
+        //Navigation key
+        public virtual Product Product { get; set; }
+    }
+}
---
+++ CenterOfCeramic/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CenterOfCeramic.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Price { get; set; }
+        public string Description { get; set; }
+
+        //Foreign key
+        public int CategoryId { get; set; }
+
+        //Navigation props
+        public virtual ICollection<Photo> Photos { get; set; }
+        public virtual Category Category { get; set; }
+
+        public Product()
+        {
+            Photos = new HashSet<Photo>();
+        }
+    }
+}
